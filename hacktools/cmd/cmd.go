d93a780de9a956d5688168763929fe19665bbd95
Gr%1m <1299091518@qq.com>
2024-09-15 10:47:52 +0800

    fix

--- hacktools/cmd/cmd.go
+++ hacktools/cmd/cmd.go
@@ -6,34 +6,37 @@ import (
 	"strings"
 )
 
-type Cmder interface {
-	Run() error
-}
-
-type Cmd struct {
-	Command []string
-
-	Stdout, Stderr bytes.Buffer
-}
-
-func (c *Cmd) Run() error {
-	cmd := exec.Command(c.Command[0], c.Command[1:]...)
-	cmd.Stdout = &c.Stdout
-	cmd.Stderr = &c.Stderr
-
-	return cmd.Run()
-}
-
 func Run(cmd string) (Stdout, Stderr string) {
 
-	c := &Cmd{
-		Command: strings.Split(cmd, " "),
+	if strings.Contains(cmd, "|") {
+		c1c2str := strings.Split(cmd, "|")
+		c1 := strings.Split(c1c2str[0], " ")
+		c2 := strings.Split(c1c2str[1], " ")
+
+		var stdout, stderr bytes.Buffer
+		r1 := exec.Command(c1[0], c1...)
+		r2 := exec.Command(c2[0], c2...)
+		r2.Stdin, _ = r1.StdoutPipe()
+		r2.Stdout = &stdout
+		r2.Stderr = &stderr
+		// err := c.Run()
+		_ = r2.Start()
+		_ = r1.Run()
+		_ = r2.Wait()
+
+		return stdout.String(), stderr.String()
 	}
 
-	err := c.Run()
+	c := strings.Split(cmd, " ")
+	runner := exec.Command(c[0], c[1:]...)
+
+	var stdout, stderr bytes.Buffer
+	runner.Stdout = &stdout
+	runner.Stderr = &stderr
+	err := runner.Run()
 	if err != nil {
 		return "", err.Error()
 	}
 
-	return c.Stdout.String(), c.Stderr.String()
+	return stdout.String(), stderr.String()
 }
